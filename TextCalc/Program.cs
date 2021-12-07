55fb25d23f623225220a0df0c2047e5666526d80
Antonio <Antonio@ASUST>
2021-12-07 10:52:57 +0000

    R

--- TextCalc/Program.cs
+++ TextCalc/Program.cs
@@ -5,20 +5,13 @@ namespace TextCalc {
 
     class Program {
 
-        enum Operators {
-            Add,
-            Multiply,
-            Subtract,
-            Divide
-        }
-
         struct Operation {
 
-            public readonly Operators Operator;
-            public readonly Double Value; 
+            public readonly String Token;
+            public readonly Double Value;
 
-            public Operation(Operators pnOperator, Double pnValue)  {
-                Operator = pnOperator;
+            public Operation(String psToken, Double pnValue) {
+                Token = psToken;
                 Value = pnValue;
             }
 
@@ -30,66 +23,86 @@ namespace TextCalc {
         static void Main(String[] _) {
             Boolean lbContinue = true;
             while (lbContinue) {
-                var lsLine = Console.ReadLine();
-                if (lsLine == "EXIT")
-                    lbContinue = false;
-                else
-                    try {
-                        _ParseLine(lsLine);
-                    } catch (Exception loException) {
-                        Console.WriteLine(loException.Message);
-                    }
+                Console.Write("> ");
+                var lsLine = $"> {Console.ReadLine()}";
+                try {
+                    if (!_ApplyOperation(lsLine))
+                        lbContinue = _ApplyCommand(lsLine);
+                } catch (Exception loException) {
+                    Console.WriteLine(loException.Message);
+                }
             }
         }
 
-        private static void _ParseLine(String psLine) {
-            if (psLine.StartsWith("MULTIPLY BY"))
-                _ApplyOperation(new Operation(Operators.Multiply, _ParseValue(psLine, "MULTIPLY BY".Length)));
-            else if (psLine.StartsWith("SUBTRACT"))
-                _ApplyOperation(new Operation(Operators.Subtract, _ParseValue(psLine, 8)));
-            else if (psLine.StartsWith("ADD"))
-                _ApplyOperation(new Operation(Operators.Add, _ParseValue(psLine, 3)));
-            else if (psLine.StartsWith("DIVIDE BY"))
-                _ApplyOperation(new Operation(Operators.Divide, _ParseValue(psLine, 9)));
-            else if (psLine.StartsWith("DISPLAY"))
-                _ApplyCommand("DISPLAY");
+        private static String[] _oOperatorTokens = new String[] {
+            "> ADD",
+            "> SUBTRACT",
+            "> MULTIPLY BY",
+            "> DIVIDE BY"
+        };
+
+        private static Boolean _ApplyOperation(String psLine) {
+            foreach (var lsToken in _oOperatorTokens)
+                if (psLine.StartsWith(lsToken))
+                    return _ApplyOperation(new Operation(lsToken, _ParseValue(psLine, lsToken.Length)));
+
+            return false;
         }
 
         private static Double _ParseValue(String psLine, Int32 pnTokenLength) {
             try {
                 return Convert.ToDouble(psLine.Substring(pnTokenLength));
             } catch {
-                throw new Exception("ERROR: while parsing value");
+                throw new Exception("ERROR: Incorrect syntax");
             }
         }
 
-        private static void _ApplyOperation(Operation poOperation) {
-            switch (poOperation.Operator) {
-                case Operators.Add:
-                    _nMem += poOperation.Value;
-                    break;
-                case Operators.Subtract:
-                    _nMem -= poOperation.Value;
-                    break;
-                case Operators.Multiply:
-                    _nMem *= poOperation.Value;
-                    break;
-                case Operators.Divide:
-                    _nMem /= poOperation.Value;
-                    break;
-            }
+        private static Boolean _ApplyOperation(Operation poOperation) {
+            try { 
+                switch (poOperation.Token) {
+                    case "> ADD":
+                        _nMem += poOperation.Value;
+                        break;
+                    case "> SUBTRACT":
+                        _nMem -= poOperation.Value;
+                        break;
+                    case "> MULTIPLY BY":
+                        _nMem *= poOperation.Value;
+                        break;
+                    case "> DIVIDE BY":
+                        _nMem /= poOperation.Value;
+                        break;
+                }
 
-            _oPartials.Enqueue(_nMem);
+                _oPartials.Enqueue(_nMem);
+
+                return true;
+            } catch {
+                throw new Exception("ERROR: while applying operation");
+            }
         }
 
-        private static void _ApplyCommand(String psCommand) {
+        private static Boolean _ApplyCommand(String psCommand) {
             switch (psCommand) {
-                case "DISPLAY":
-                    while (_oPartials.Count > 0) {
-                        _nMem = _oPartials.Dequeue();
+                case "> DISPLAY":
+                    if (_oPartials.Count == 0)
                         Console.WriteLine($"< {_nMem}");
-                    }
-                    break;
+                    else 
+                        while (_oPartials.Count > 0) {
+                            _nMem = _oPartials.Dequeue();
+                            Console.WriteLine($"< {_nMem}");
+                        }
+                    return true;
+                case "> CLEAR":
+                    _nMem = 0;
+                    _oPartials.Clear();
+                    return true;
+                case "> EXIT":
+                    return false;
+                default:
+                    Console.WriteLine("Not a valid line");
+                    return true;
+
             }
         }
 
